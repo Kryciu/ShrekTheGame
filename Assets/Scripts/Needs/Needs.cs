0571c81c76801ad95807f2696fdd3f7b55560276
Kryciu <52796524+Kryciu@users.noreply.github.com>
2023-03-30 00:51:48 +0200

    NeedsUpdate

--- Assets/Scripts/Needs/Needs.cs
+++ Assets/Scripts/Needs/Needs.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class Needs : MonoBehaviour
 {
     [SerializeField] private float Hunger = 100;
+    [SerializeField] private float Thirsty = 100;
+    [SerializeField] private float Toilet = 100;
+    [SerializeField] private float Hygiene = 100;
+    [SerializeField] private float Sleep = 100;
+    [SerializeField] private float Fun = 100;
     [SerializeField] private float WaitTime = 1.0f;
 
     void Start()
     {
         StartCoroutine(UpdateNeeds(Hunger, WaitTime));
+        StartCoroutine(UpdateNeeds(Thirsty, WaitTime));
+        StartCoroutine(UpdateNeeds(Toilet, WaitTime));
+        StartCoroutine(UpdateNeeds(Hygiene, WaitTime));
+        StartCoroutine(UpdateNeeds(Sleep, WaitTime));
+        StartCoroutine(UpdateNeeds(Fun, WaitTime));
+
     }
 
     IEnumerator UpdateNeeds(float Input, float Wait)
